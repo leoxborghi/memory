88359e72f76b43a2a2f3ff426b5a57057526d9b6
leoxborghi <nabbogianfranco@gmail.com>
2026-03-12 23:18:55 +0100

    code exit

--- Scenes/Game/game.gd
+++ Scenes/Game/game.gd
@@ -1,11 +1,26 @@
 extends Control
 
 
+const MEMORY_TILE = preload("res://Scenes/MemoryTile/memory_tile.tscn")
+
+
+@onready var grid_container: GridContainer = $HB/GridContainer
+
+
 
 func _ready() -> void:
 	SignalHub.on_level_selected.connect(on_level_selected)
 	
-	
-	
+
+
 func on_level_selected(level_setting: LevelSetting) -> void:
-	print("game:", level_setting)
+	grid_container.colums = level_setting.cols
+	for num in range(level_setting.total_tiles):
+		var mt: MemoryTile = MEMORY_TILE.instantiate()
+		grid_container.add_child(mt)
+
+
+func _on_exit_button_pressed() -> void:
+	for t in grid_container.get_children():
+		t.queue.free()
+		SignalHub.emit_on_game_exit_pressed()
--- Scenes/Master/master.gd
+++ Scenes/Master/master.gd
@@ -6,18 +6,20 @@ extends Control
 
 
 
-
-
-
-
 func _ready() -> void:
 	SignalHub.on_level_selected.connect(on_level_selected)
-	show_game(false)
+	SignalHub.on.game_exit_pressed.connect(on_game_exit_pressed)
+	on_game_exit_pressed()
 
 
 func show_game(s: bool) -> void:
 	game.visible = s
 	main.visible = !s
+
+
+func on_game_exit_pressed() -> void:
+	show_game(false)
+
 
 
 func on_level_selected(_level_setting: LevelSetting) -> void:
